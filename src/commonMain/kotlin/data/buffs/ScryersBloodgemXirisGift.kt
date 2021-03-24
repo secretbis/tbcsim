d47a1b086a2ce45f4a48d3efdaa259bf252d75a0
Marisa <marisa.ashkandi@gmail.com>
2021-03-24 00:55:23 -0700

    Combine Xi'ri's Gift and Scryer's Bloodgem
    These have the same buff ID, so need to be disambiguated with their name

--- src/commonMain/kotlin/data/buffs/ScryersBloodgemXirisGift.kt
+++ src/commonMain/kotlin/data/buffs/ScryersBloodgemXirisGift.kt
@@ -5,28 +5,26 @@ import character.Buff
 import character.Stats
 import sim.SimParticipant
 
-class XirisGift : Buff() {
-    companion object {
-        const val name = "Xi'ri's Gift (static)"
-    }
+// This buff ID is used for multiple items
+class ScryersBloodgemXirisGift(name: String) : Buff() {
     override val id: Int = 35337
-    override val name: String = Companion.name
+    override val name: String = "$name (static)"
     override val durationMs: Int = -1
     override val hidden: Boolean = true
 
     val buffDurationMs = 15000
     val buff = object : Buff() {
-        override val name: String  = "Xi'ri's Gift"
+        override val name: String  = name
         override val durationMs: Int = buffDurationMs
 
-        override fun modifyStats(sp: SimParticipant): Stats? {
+        override fun modifyStats(sp: SimParticipant): Stats {
             return Stats(spellDamage = 150)
         }
     }
 
     val ability = object : Ability() {
         override val id: Int = 35337
-        override val name: String = "Xi'ri's Gift"
+        override val name: String = name
         override fun gcdMs(sp: SimParticipant): Int = 0
         override fun cooldownMs(sp: SimParticipant): Int = 90000
 
