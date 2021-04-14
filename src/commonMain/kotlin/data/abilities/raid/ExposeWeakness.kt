308dbc4a443eb7b0ba3a968b1494672fa8c78b40
Marisa <marisa.ashkandi@gmail.com>
2021-04-13 23:04:54 -0700

    Add Expose Weakness as a raid debuff
    Resolves #33

---
+++ src/commonMain/kotlin/data/abilities/raid/ExposeWeakness.kt
@@ -0,0 +1,32 @@
+package data.abilities.raid
+
+import character.Ability
+import character.Buff
+import character.Stats
+import sim.SimParticipant
+
+class ExposeWeakness(val agility: Int): Ability() {
+    override val id: Int = 34501
+    override val name: String = "Expose Weakness ($agility Agi)"
+    override fun gcdMs(sp: SimParticipant): Int = 0
+
+    val buff = object : Buff() {
+        override val name: String = "Expose Weakness ($agility Agi)"
+        // Assume the caster is always maintaining this
+        override val durationMs: Int = -1
+        override val hidden: Boolean = true
+
+        // Assume a GoA uptime of about 80% when twisting
+        // Also assume the caster has Enhancing Totems
+        override fun modifyStats(sp: SimParticipant): Stats {
+            return Stats(
+                attackPower = (0.25 * agility).toInt(),
+                rangedAttackPower = (0.25 * agility).toInt()
+            )
+        }
+    }
+
+    override fun cast(sp: SimParticipant) {
+        sp.sim.addRaidBuff(buff)
+    }
+}
