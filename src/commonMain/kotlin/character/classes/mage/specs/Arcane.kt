dbaac2dec372b18e012043694b69f614c2239c31
Joshua Feingold <jjfeingold@gmail.com>
2021-12-26 12:05:06 -0500

    Improvements to the arcane mage EP calculation.

--- src/commonMain/kotlin/character/classes/mage/specs/Arcane.kt
+++ src/commonMain/kotlin/character/classes/mage/specs/Arcane.kt
@@ -2,11 +2,14 @@ package character.classes.mage.specs
 
 import character.Spec
 import character.SpecEpDelta
+import character.Stats
 
 class Arcane : Spec() {
     override val name: String = "Arcane"
     override val epBaseStat: SpecEpDelta = spellPowerBase
-    override val epStatDeltas: List<SpecEpDelta> = defaultCasterDeltas
+    override val epStatDeltas: List<SpecEpDelta> = listOf(Triple("spirit", Stats(spirit = 50), 50.0)) +
+            defaultCasterDeltas
+
 
     override fun redSocketEp(deltas: Map<String, Double>): Double {
         // 12 spell dmg
@@ -15,7 +18,7 @@ class Arcane : Spec() {
 
     override fun yellowSocketEp(deltas: Map<String, Double>): Double {
         // 5 spell haste rating / 6 spell damage
-        return ((deltas["spellHasteRating"] ?: 0.0) * 5.0) + 6.0
+        return ((deltas["intellect"] ?: 0.0) * 10.0)
     }
 
     override fun blueSocketEp(deltas: Map<String, Double>): Double {
