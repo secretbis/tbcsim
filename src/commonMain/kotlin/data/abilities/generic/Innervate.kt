bd0e42c5377582ca10a4b6a76f78bd25fc9808a7
Joshua Feingold <jjfeingold@gmail.com>
2021-12-26 11:55:50 -0500

    Add Innervate.

---
+++ src/commonMain/kotlin/data/abilities/generic/Innervate.kt
@@ -0,0 +1,35 @@
+package data.abilities.generic
+
+import character.*
+import mechanics.General
+import mechanics.Rating
+import sim.SimParticipant
+import kotlin.random.Random
+
+class Innervate : Ability() {
+    companion object {
+        const val name = "Innervate"
+    }
+
+    override val id: Int = 29166
+    override val name: String = Companion.name
+    override val icon: String = "spell_nature_lightning.jpg"
+    override fun gcdMs(sp: SimParticipant): Int = 0
+    override val castableOnGcd = true
+    override fun cooldownMs(sp: SimParticipant): Int = 720000
+
+    val buff = object : Buff() {
+        override val name: String = Companion.name
+        override val icon: String = "spell_nature_lightning.jpg"
+        override val durationMs: Int = 20000
+
+        //NOTE: This assumes arcane meditation and mage armor. Unsure how to detect actual value.
+        override fun modifyStats(sp: SimParticipant): Stats {
+                return Stats(manaPer5Seconds = (General.mp5FromSpiritNotCasting(sp) * 4.4).toInt())
+        }
+    }
+
+    override fun cast(sp: SimParticipant) {
+        sp.addBuff(buff)
+    }
+}
